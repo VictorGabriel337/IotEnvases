2784ddf206328c673d93d3006c26f6ee7b04b33d
Victor Souza <victor.abrao337@gmail.com>
2025-02-04 18:50:59 -0300

    deploy socorro

--- proxy.py
+++ proxy.py
@@ -1,25 +1,22 @@
 from flask import Flask, jsonify
+from flask_cors import CORS  # Importando CORS
 import requests
+import os
 
 app = Flask(__name__)
+CORS(app)  # Habilita CORS para todas as origens
 
-ESP_IP = "http://192.168.15.7:80/"  # IP do ESP8266 na rede local
+ESP_IP = "http://192.168.15.7:80/"  # IP do ESP8266
 
 @app.route('/api', methods=['GET'])
 def get_sensor_data():
     try:
         response = requests.get(ESP_IP, timeout=5)  # Requisição ao ESP8266
-        response.raise_for_status()  # Levanta erro se houver falha
-        return jsonify(response.json())  # Retorna os dados recebidos para a aplicação
+        response.raise_for_status()
+        return jsonify(response.json())  # Retorna os dados
     except requests.exceptions.RequestException as e:
         return jsonify({"error": "Falha ao conectar ao ESP8266", "details": str(e)}), 500
 
-# if __name__ == '__main__':
-#     app.run(host="0.0.0.0", port=5000)
-
-
-import os
-
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Usa a porta definida pelo Railway ou 5000 como fallback
+    port = int(os.environ.get("PORT", 5000))  # Porta do Railway
     app.run(host="0.0.0.0", port=port)
