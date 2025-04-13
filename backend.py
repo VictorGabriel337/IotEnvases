7585e2bedb6044f3db787c2e5175ba78068bae44
Victor Souza <victor.abrao337@gmail.com>
2025-04-13 20:35:06 -0300

    alterado

--- backend.py
+++ backend.py
@@ -4,7 +4,7 @@ import threading
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, origins=["*"])  # Permitir CORS para todas as origens
+CORS(app)
 app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 
 # Variáveis globais para guardar os dados mais recentes
