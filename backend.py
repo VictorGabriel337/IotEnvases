dc2c36de5b00b0a926b6982d41a93d80b0064b4f
Victor Souza <victor.abrao337@gmail.com>
2025-04-13 20:05:11 -0300

    alterado

--- backend.py
+++ backend.py
@@ -4,7 +4,7 @@ import threading
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://iotenvases.netlify.app"}})
+CORS(app)
 app.run(host='0.0.0.0', port=5000, debug=True)
 
 # Variáveis globais para guardar os dados mais recentes
