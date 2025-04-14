8a18ba0ffaaab5e2f91190beb6358cc1ede33037
Victor Souza <victor.abrao337@gmail.com>
2025-04-14 17:03:06 -0300

    alterado

--- backend.py
+++ backend.py
@@ -13,15 +13,15 @@ def home():
     return send_from_directory(os.path.join(app.root_path, 'Envases', 'Dashboard'), 'Dashboard.html')
 
 
-@app.route("/sensores")
-def sensores():
-    # Aqui você pode conectar com MQTT, banco de dados ou variáveis mockadas
-    # Por enquanto vamos usar valores simulados (só pra teste)
-    return jsonify({
-        "lowSignalCount": 15,
-        "cadenceTotalTime": 120,       # em segundos
-        "nonCadenceTotalTime": 30      # em segundos
-    })
+# @app.route("/sensores")
+# def sensores():
+#     # Aqui você pode conectar com MQTT, banco de dados ou variáveis mockadas
+#     # Por enquanto vamos usar valores simulados (só pra teste)
+#     return jsonify({
+#         "lowSignalCount": 15,
+#         "cadenceTotalTime": 120,       # em segundos
+#         "nonCadenceTotalTime": 30      # em segundos
+#     })
 
 latest_status = {}
 status_lock = threading.Lock()
