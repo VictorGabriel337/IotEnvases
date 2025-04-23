608b2be5bae5dff5a52648f8aa0966b35c5df01d
Victor Souza <victor.abrao337@gmail.com>
2025-04-22 21:06:16 -0300

    alterado

--- backend.py
+++ backend.py
@@ -51,12 +51,20 @@ def sensores():
 
 
 def mqtt_thread():
-    print("Iniciando conexão MQTT...")
+    print(" Iniciando conexão MQTT...")
     mqtt_client = mqtt.Client()
     mqtt_client.username_pw_set("Iotenvases", "Iotenvases42")
     mqtt_client.tls_set()
-    mqtt_client.connect("534dc0a4d7544a60a30022826acda692.s1.eu.hivemq.cloud", 8883)
+
+    try:
+        mqtt_client.connect("534dc0a4d7544a60a30022826acda692.s1.eu.hivemq.cloud", 8883)
+        print(" Conectado ao broker MQTT com sucesso.")
+    except Exception as e:
+        print(" Erro ao conectar no MQTT:", e)
+        return
+
     mqtt_client.subscribe("machine/status")
+    print(" Subscrito no tópico machine/status")
     mqtt_client.on_message = on_message
     mqtt_client.loop_forever()
 
@@ -70,4 +78,4 @@ threading.Thread(target=mqtt_thread).start()
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
     app.run(debug=True)
-    
+
