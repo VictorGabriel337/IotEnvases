fe5bf04b472a0cf86a357f13be552fa50528af4e
Victor Souza <victor.abrao337@gmail.com>
2025-06-03 17:54:31 -0300

    alterado 03.06

--- backend.py
+++ backend.py
@@ -69,4 +69,6 @@ def get_status():
     return jsonify(latest_status)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # usa a porta do Render se existir
+    app.run(host="0.0.0.0", port=port)
